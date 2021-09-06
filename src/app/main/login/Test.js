54f48857a0895e58278d5c088cdfac16c28fec27
Matsushita Shota <ms@MatsushitanoMacBook-Pro.local>
2021-09-07 05:57:17 +0900

    reducer　作成中

--- src/app/main/login/Test.js
+++ src/app/main/login/Test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import { getTest, selectTest } from "../store/testSlice";
@@ -7,8 +7,16 @@ function Test(props) {
     const dispatch = useDispatch();
     const test = useSelector(selectTest);
 
+    useEffect(() => {
+        console.log("useeffect");
+        dispatch(getTest());
+    });
+
     return (
         <>
+            {test.map(item => (
+                <p>item.title</p>
+            ))}
             <Button>
                 Button
             </Button>
--- src/app/main/store/testSlice.js
+++ src/app/main/store/testSlice.js
@@ -1,10 +1,10 @@
 import {createSlice, createAsyncThunk, createEntityAdapter} from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getTest = createAsyncThunk('practice3/login', async params => {
-    const response = await axios.get(`/test`);
-    return response.data;
-})
+export const getTest = createAsyncThunk('test', async () => {
+    const response = await axios.get(`http://localhost:8000/test`);
+    return response.data.data;
+});
 
 const testAdapter = createEntityAdapter({});
 
